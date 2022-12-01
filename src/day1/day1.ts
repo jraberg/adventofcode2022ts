284a76046751d75ba03432b048e6f902a89f7dc1
Jonas Råberg <jraberg@users.noreply.github.com>
2022-12-01 18:38:38 +0100

    feat: day1

---
+++ src/day1/day1.ts
@@ -0,0 +1,11 @@
+import * as fs from 'fs';
+import path from 'path';
+
+const testDataPath = path.join(__dirname, 'puzzle.in.txt');
+const caloriesByDeer = fs.readFileSync(testDataPath, {encoding: 'utf8'}).trim().split('\n\n')
+                   .map(deer => deer.split('\n').map(c => parseInt(c)))
+                   .map(calories => calories.reduce((acc, calorie) => acc + calorie, 0))
+                         .sort((a, b) => b - a);
+
+console.log(caloriesByDeer[0]);
+console.log(caloriesByDeer.slice(0,3).reduce((acc, calorie) => acc + calorie, 0));
