2626957da533e754df10b1361524af71536c737b
Jonas Råberg <jraberg@users.noreply.github.com>
2022-12-06 08:12:31 +0100

    feat: day6

---
+++ src/day6/day6.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs';
+import path from 'path';
+
+const input = fs.readFileSync(path.join(__dirname, 'day6.puzzle.in.txt'), { encoding: 'utf-8' }).trim();
+const isUniqueChars = (s: string, noUniqueChars:number) =>  (new Set(s)).size === noUniqueChars;
+
+const firstPositionAfterUniqChars = (s: string, noUniqueChars: number) => {
+    let i = 0;
+    while (i < s.length && !isUniqueChars(s.slice(i, i + noUniqueChars), noUniqueChars)) {
+        i++;
+    }
+    return i+noUniqueChars;
+}
+
+const solutionPart1 = firstPositionAfterUniqChars(input, 4);
+console.log(`Part 1 - solution: ${solutionPart1}`);
+const solutionPart2 = firstPositionAfterUniqChars(input, 14);
+console.log(`Part 2 - solution: ${solutionPart2}`);
