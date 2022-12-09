c6091f0a8fd140d3cd3e77d1146783910c325ec2
Jonas Råberg <jraberg@users.noreply.github.com>
2022-12-09 15:59:23 +0100

    refactor: improve naming

--- src/day5/day5.ts
+++ src/day5/day5.ts
@@ -1,23 +1,25 @@
 import path from 'path';
 import * as fs from 'fs';
 
-type StackMove = { move: number; from: number; to: number };
+type StackMove = { quantity: number; from: number; to: number };
 
 class Stack {
-  private stackMoves: StackMove[] = [];
+  private instructions: StackMove[] = [];
   private stack: string[][] = [];
 
   constructor(private rawStack: string, rawMoves: string) {
     this.parseStack(rawStack);
-    this.parseStackMoves(rawMoves);
+    this.parseInstructions(rawMoves);
   }
 
   getMessages = (): string => this.stack.map((s) => s[s.length - 1]).join('');
 
-  moveStackItemsOneByOne = () => this.stackMoves.forEach((stackMove) => this.moveItems(stackMove.move, stackMove.from, stackMove.to));
-  moveStackItemsInOneMove = () => this.stackMoves.forEach((stackMove) => this.moveItemsInOneMove(stackMove.move, stackMove.from, stackMove.to));
+  moveStackItemsOneByOne = () => this.instructions.forEach((instruction) => this.moveItems(instruction.quantity, instruction.from, instruction.to));
+
+  moveStackItemsInOneMove = () => this.instructions.forEach((instruction) => this.moveItemsInOneMove(instruction.quantity, instruction.from, instruction.to));
 
   private moveItems = (move: number, from: number, to: number) => this.stack[to - 1].push(...this.stack[from - 1].splice(this.stack[from - 1].length - move, move).reverse());
+
   private moveItemsInOneMove = (move: number, from: number, to: number) => {
     const items = this.stack[from - 1].splice(this.stack[from - 1].length - move, move);
     if (move > 1) {
@@ -32,7 +34,7 @@ class Stack {
     const colIndexes = raw.trim().split('\n').slice(-1)[0].split('  ').map(Number);
     this.stack = [...colIndexes.map(_ => [])];
     for (const colIndex of colIndexes) {
-      s.forEach((l, i) => {
+      s.forEach((l) => {
         const c = l.slice((colIndex - 1) * 4, (colIndex - 1) * 4 + 3);
         if (c.trim()) {
           this.stack[colIndex - 1].unshift(c.replace('[', '').replace(']', ''));
@@ -41,10 +43,10 @@ class Stack {
     }
   }
 
-  private parseStackMoves = (rawMoves: string): StackMove[] =>
-    this.stackMoves = rawMoves.trim().split('\n').map((line) => {
-      const m = /move (?<move>\d+) from (?<from>\d+) to (?<to>\d+)/.exec(line);
-      return m && m.groups ? { move: Number(m.groups.move), from: Number(m.groups.from), to: Number(m.groups.to), } : { move: 0, from: 0, to: 0 };
+  private parseInstructions = (rawMoves: string): StackMove[] =>
+    this.instructions = rawMoves.trim().split('\n').map((line) => {
+      const m = /move (?<quantity>\d+) from (?<from>\d+) to (?<to>\d+)/.exec(line);
+      return m && m.groups ? { quantity: Number(m.groups.quantity), from: Number(m.groups.from), to: Number(m.groups.to), } : { quantity: 0, from: 0, to: 0 };
     });
 
 }
